8d14e210104a3277de801e96ad1d3c91e84ff621
rocco <rocco@hackerstribe.com>
2018-01-24 22:42:43 +0100

    respond with ascii art

---
+++ index.js
@@ -0,0 +1,28 @@
+const colors = require('colors')
+const {logo, welcomeText} = require('./other/text')
+const NetcatServer = require('netcat/server')
+const readline = require('readline')
+const CliInterface = require('./lib/interface')
+
+const QUEENBEE_PORT = process.env.QUEENBEE_PORT || 8888
+const BEES_PORT = process.env.BEES_PORT || 2389
+const HOST = '127.0.0.1' // only localhost interface
+
+const welcomeMsg = `${logo}\n${welcomeText}`.yellow
+
+console.log(logo.yellow)
+
+// QUEEN BEE
+let nc = new NetcatServer()
+nc.k().address(HOST).port(QUEENBEE_PORT).listen().on('connection', (queenBeeSocket) => {
+  let cli = new CliInterface({welcomeMsg, stdin: queenBeeSocket, stdout: queenBeeSocket})
+  cli.start()
+})
+
+// HIVE
+let hive = new NetcatServer()
+hive.k().port(BEES_PORT).listen().on('connection', (bee) => {
+
+})
+
+// hive.getClients()
