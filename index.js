ab3734859423a807d90c4a3b46c2f30c3a87167e
rocco <rocco@hackerstribe.com>
2018-01-29 00:18:40 +0100

    improve order

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const colors = require('colors')
 const {logo, welcomeText} = require('./other/text')
 const NetcatServer = require('netcat/server')
 const readline = require('readline')
-const CliInterface = require('./lib/interface')
+const HiveInterface = require('./lib/hive')
 
 const QUEENBEE_PORT = process.env.QUEENBEE_PORT || 8888
 const BEES_PORT = process.env.BEES_PORT || 2389
@@ -12,17 +12,17 @@ const welcomeMsg = `${logo}\n${welcomeText}`.yellow
 
 console.log(logo.yellow, `\nAlveare started on port ${QUEENBEE_PORT}, waiting for bees on port ${BEES_PORT}`.cyan)
 
-// QUEEN BEE
-let nc = new NetcatServer()
-nc.k().address(HOST).port(QUEENBEE_PORT).listen().on('connection', (queenBeeSocket) => {
-  let cli = new CliInterface({welcomeMsg, stdin: queenBeeSocket, stdout: queenBeeSocket})
-  cli.start()
-})
-
-// HIVE
+// BEE HIVE
 let hive = new NetcatServer()
 hive.k().port(BEES_PORT).listen().on('connection', (bee) => {
-
+  console.log(`New bee ${bee.id} connected`.yellow)
 })
 
 // hive.getClients()
+
+// QUEEN BEE
+let nc = new NetcatServer()
+nc.k().address(HOST).port(QUEENBEE_PORT).listen().on('connection', (queenBee) => { // admin socket
+  let cli = new HiveInterface({welcomeMsg, socket: queenBee})
+  cli.start()
+})
